d07a26e08c8dde8c2d01429664b0c3df61cdbc6f
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2024-02-18 10:04:00 +0100

    add missing includes

--- include/grv/grv.h
+++ include/grv/grv.h
@@ -1,10 +1,15 @@
 #ifndef GRV_H
 #define GRV_H
 
+#include "grv_common.h"
 #include "grv_base.h"
 #include "grv_str.h"
+#include "grv_strarr.h"
 #include "grv_cstr.h"
 #include "grv_util.h"
 #include "grv_fs.h"  
+#include "grv_log.h"
+#include "grv_memory.h"
 
 #endif
+
