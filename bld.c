7567ee9fa7984b9a986fd2c58643c5610b8db86d
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2024-02-18 15:25:45 +0100

    update bld

--- bld.c
+++ bld.c
@@ -1,10 +1,9 @@
 #include "grvbld.h"
 
-#ifdef GRV_BUILD_CONFIGURED
-
 int main(int argc, char** argv) {
     GRV_CHECK_AND_REBUILD();    
     grvbld_config_t* config =  grvbld_config_new(argc, argv);
+    grvbld_strarr_push(&config->warnings, "-Wextra -Wpedantic");
 
     grvbld_target_t* libgrv = grvbld_target_create("grv", GRVBLD_STATIC_LIBRARY);
     grvbld_target_add_src(libgrv, "src/grv.c");
@@ -13,6 +12,7 @@ int main(int argc, char** argv) {
     grvbld_target_t* libgrvgfx = grvbld_target_create("grvgfx", GRVBLD_STATIC_LIBRARY);
     grvbld_target_add_src(libgrvgfx, "src/grv_gfx/grv_gfx.c");
     grvbld_target_link_library(libgrvgfx, "SDL2");
+    grvbld_target_add_data_file(libgrvgfx, "src/grv_gfx/cozette.psf");
     grvbld_build_target(config, libgrvgfx);
 
     grvbld_run_tests(config);
@@ -33,10 +33,9 @@ int main(int argc, char** argv) {
     grvbld_target_add_src(test_window, "src/grv_gfx/test_window.c");
     grvbld_target_link(test_window, libgrv);
     grvbld_target_link(test_window, libgrvgfx);
-    //test_window->run_a//fter_build = true;
+    //test_window->run_after_build = true;
     grvbld_build_target(config, test_window);
 
     return 0;
 }
 
-#endif
