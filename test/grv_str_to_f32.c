dac731643e67518dfc6aed4e988faff21cec00ce
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2024-02-19 22:50:29 +0100

    add more test cases

--- test/grv_str_to_f32.c
+++ test/grv_str_to_f32.c
@@ -9,4 +9,7 @@ GRV_TEST_BEGIN_NEW(grv_str_to_f32)
     GRV_TEST_EQUAL_F32(grv_str_to_f32(grv_str_ref("+3.14e2")), 314.0f);
     GRV_TEST_EQUAL_F32(grv_str_to_f32(grv_str_ref("+3.14e-2")), 0.0314f);
     GRV_TEST_EQUAL_F32(grv_str_to_f32(grv_str_ref("+3.14e-2abc")), 0.0314f);
+    GRV_TEST_EQUAL_F32(grv_str_to_f32(grv_str_ref("-55.")), -55.0f);
+    GRV_TEST_EQUAL_F32(grv_str_to_f32(grv_str_ref("55.")), 55.0f);
+    GRV_TEST_EQUAL_F32(grv_str_to_f32(grv_str_ref("-256")), -256.0f);
 GRV_TEST_END_NEW()
