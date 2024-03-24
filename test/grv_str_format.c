02cae2b046f7f2d35a64d13e9648a1ad8dc6c02f
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2024-03-24 12:01:32 +0100

    add test for grv_str_format

---
+++ test/grv_str_format.c
@@ -0,0 +1,8 @@
+#include "grv/grv_str.h"
+#include "grv/grv_test.h"
+#include "grv/grv.h"
+
+GRV_TEST_BEGIN_NEW(grv_str_format)
+    grv_str_t s = grv_str_new_with_format("{int}", 42);
+    GRV_TEST_EQUAL_STR(s, "42");
+GRV_TEST_END_NEW()
