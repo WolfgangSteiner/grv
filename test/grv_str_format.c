af9aa4d80b08d54997fc04da5206014095c7ff7a
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2024-03-24 12:01:32 +0100

    fix test for grv_str_format

--- test/grv_str_format.c
+++ test/grv_str_format.c
@@ -3,6 +3,6 @@
 #include "grv/grv.h"
 
 GRV_TEST_BEGIN_NEW(grv_str_format)
-    grv_str_t s = grv_str_new_with_format("{int}", 42);
+    grv_str_t s = grv_str_format(grv_str_ref("{int}"), 42);
     GRV_TEST_EQUAL_STR(s, "42");
 GRV_TEST_END_NEW()
