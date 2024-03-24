9aa73ef2df668d6f7f66b01dcd92d6efe8da1ebb
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2024-03-24 12:01:32 +0100

    fix indentation

--- src/grv_util.c
+++ src/grv_util.c
@@ -40,35 +40,35 @@ int grv_system(grv_str_t cmd) {
 }
 
 grv_strarr_t grv_system_with_capture(grv_str_t cmd) {
-  char* cmd_cstr = grv_str_cstr(cmd);
-  grv_strarr_t result = grv_system_with_capture_cstr(cmd_cstr); 
-  grv_free(cmd_cstr);
-  return result;
+    char* cmd_cstr = grv_str_cstr(cmd);
+    grv_strarr_t result = grv_system_with_capture_cstr(cmd_cstr); 
+    grv_free(cmd_cstr);
+    return result;
 }
 
-grv_strarr_t grv_system_with_capture_cstr(char* cmd) {  
-  grv_strarr_t result = grv_strarr_new();
-  FILE* fp = popen(cmd, "r");
-  if (fp == NULL) {
+grv_strarr_t grv_system_with_capture_cstr(char* cmd) {
+    grv_strarr_t result = grv_strarr_new();
+    FILE* fp = popen(cmd, "r");
+    if (fp == NULL) {
+        return result;
+    }
+
+    char* line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    while ((read = getline(&line, &len, fp)) != -1) {
+        grv_str_t line_str = grv_str_ref(line);
+        line_str = grv_str_remove_trailing_newline(line_str); 
+        grv_strarr_push(&result, grv_str_copy(line_str));
+        grv_str_free(&line_str);
+    }
+
+    pclose(fp);
+    if (line) {
+        free(line);
+    }
+
     return result;
-  }
-
-  char* line = NULL;
-  size_t len = 0;
-  ssize_t read;
-  while ((read = getline(&line, &len, fp)) != -1) {
-    grv_str_t line_str = grv_str_ref(line);
-    line_str = grv_str_remove_trailing_newline(line_str); 
-    grv_strarr_push(&result, grv_str_copy(line_str));
-    grv_str_free(&line_str);
-  }
-
-  pclose(fp);
-  if (line) {
-    free(line);
-  }
-
-  return result;
 }
 
 pid_t grv_fork(grv_str_t cmd, grv_strarr_t* args) {
@@ -86,23 +86,23 @@ pid_t grv_fork(grv_str_t cmd, grv_strarr_t* args) {
 }
 
 grv_strarr_t grv_util_glob(grv_str_t pattern) {
-  // iterate over contents of directory
-  grv_strarr_t result = grv_strarr_new();
-  char* pattern_cstr = grv_str_copy_to_cstr(pattern);
-  glob_t glob_result;
-  glob(pattern_cstr, GLOB_TILDE, NULL, &glob_result);
-  if (glob_result.gl_pathc == 0) {
-    globfree(&glob_result);
-    return result;
-  }
+    // iterate over contents of directory
+    grv_strarr_t result = grv_strarr_new();
+    char* pattern_cstr = grv_str_copy_to_cstr(pattern);
+    glob_t glob_result;
+    glob(pattern_cstr, GLOB_TILDE, NULL, &glob_result);
+    if (glob_result.gl_pathc == 0) {
+        globfree(&glob_result);
+        return result;
+    }
 
-  for (size_t i = 0; i < glob_result.gl_pathc; ++i) {
-    grv_str_t path = grv_str_new(glob_result.gl_pathv[i]);
-    grv_strarr_push(&result, path);
-  }
-  globfree(&glob_result);
-  grv_free(pattern_cstr);
-  return result;
+    for (size_t i = 0; i < glob_result.gl_pathc; ++i) {
+        grv_str_t path = grv_str_new(glob_result.gl_pathv[i]);
+        grv_strarr_push(&result, path);
+    }
+    globfree(&glob_result);
+    grv_free(pattern_cstr);
+    return result;
 }
 
 #define GRV_UTIL_EPOCH 1679604975
@@ -144,36 +144,36 @@ u64 grv_random_u64(void) {
 }
 
 bool grv_cmd_available(grv_str_t cmd) {
-  grv_str_t check_cmd = grv_str_new("which ");
-  grv_str_append_str(&check_cmd, cmd);
-  grv_str_append_cstr(&check_cmd, " > /dev/null 2>&1");
-  int result = system(grv_str_cstr(check_cmd));
-  grv_str_free(&check_cmd);
-  return result == 0;
+    grv_str_t check_cmd = grv_str_new("which ");
+    grv_str_append_str(&check_cmd, cmd);
+    grv_str_append_cstr(&check_cmd, " > /dev/null 2>&1");
+    int result = system(grv_str_cstr(check_cmd));
+    grv_str_free(&check_cmd);
+    return result == 0;
 }
 
 struct tm grv_local_time(void) {
-  time_t t = time(NULL);
-  return *localtime(&t);
+    time_t t = time(NULL);
+    return *localtime(&t);
 }
 
 grv_str_t grv_local_time_str(void) {
-  struct tm tm = grv_local_time();
-  grv_str_t result = grv_str_new_with_format("%02d:%02d:%02d", tm.tm_hour, tm.tm_min, tm.tm_sec);
-  return result;
+    struct tm tm = grv_local_time();
+    grv_str_t result = grv_str_new_with_format("%02d:%02d:%02d", tm.tm_hour, tm.tm_min, tm.tm_sec);
+    return result;
 }
 
 grv_str_t grv_local_datetime_str() {
-  struct tm tm = grv_local_time();
-  grv_str_t result = grv_str_new_with_format(
-    "%d%02d%02dT%02d%02d%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
-  return result;
+    struct tm tm = grv_local_time();
+    grv_str_t result = grv_str_new_with_format(
+        "%d%02d%02dT%02d%02d%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    return result;
 }
 
 f32 grv_local_time_f32(void) {
-  struct tm tm = grv_local_time();
-  f32 result = (f32)tm.tm_hour + (f32)tm.tm_min / 60.0f + (f32)tm.tm_sec / 3600.0f;
-  return result;
+    struct tm tm = grv_local_time();
+    f32 result = (f32)tm.tm_hour + (f32)tm.tm_min / 60.0f + (f32)tm.tm_sec / 3600.0f;
+    return result;
 }
 
 grv_strarr_t grv_readlines(grv_str_t filename) {
