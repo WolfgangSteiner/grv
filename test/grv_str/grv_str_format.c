3edeef85d29cd03202e2eb624313cb30b87436e5
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2024-04-09 18:00:09 +0200

    add additional test

--- test/grv_str/grv_str_format.c
+++ test/grv_str/grv_str_format.c
@@ -1,8 +1,9 @@
 #include "grv/grv_str.h"
 #include "grv/grv_test.h"
-#include "grv/grv.h"
 
 GRV_TEST_BEGIN_NEW(grv_str_format)
     grv_str_t s = grv_str_format(grv_str_ref("{int}"), 42);
     GRV_TEST_EQUAL_STR(s, "42");
+    s = grv_str_format(grv_str_ref("{str} {str} {str}"), grv_str_ref("one"), grv_str_ref("two"), grv_str_ref("three"));
+    GRV_TEST_EQUAL_STR(s, "one two three");
 GRV_TEST_END_NEW()
