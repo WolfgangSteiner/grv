0d0fbb8899d39498ec6b1e527575f87b16e3c42f
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2025-04-05 11:22:14 +0200

    add rect_fx32 functions

--- include/grv_gfx/rect_fx32.h
+++ include/grv_gfx/rect_fx32.h
@@ -18,6 +18,9 @@ GRV_INLINE rect_fx32 rect_fx32_from_i32(i32 x, i32 y, i32 w, i32 h) {
     };
 }
 
+GRV_INLINE vec2_fx32 rect_fx32_pos(rect_fx32 rect) { return (vec2_fx32){.x=rect.x, .y=rect.y}; }
+GRV_INLINE vec2_fx32 rect_fx32_size(rect_fx32 rect) { return (vec2_fx32){.x=rect.w, .y=rect.h}; }
+
 GRV_INLINE rect_i32 rect_fx32_round(rect_fx32 rect) {
 	return (rect_i32){
 		.x=fx32_round(rect.x),
@@ -50,6 +53,16 @@ GRV_INLINE bool rect_fx32_intersect(rect_fx32 a, rect_fx32 b) {
     i32 b_y1 = b.y.val;
     i32 b_y2 = b.y.val + b.h.val;
     return !(a_x2 < b_x1 || a_x1 > b_x2 || a_y2 < b_y1 || a_y1 > b_y2);
+}
+
+GRV_INLINE rect_fx32 rect_fx32_lower_part(rect_fx32 rect, fx32 factor) {
+    fx32 h = fx32_mul(rect.h, factor);
+    return (rect_fx32) {
+        .x = rect.x,
+        .y = fx32_add(rect.y, fx32_sub(rect.h, h)),
+        .w = rect.w,
+        .h = h
+    };
 }
 
 #endif
