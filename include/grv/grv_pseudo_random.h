37d2113395a2ac4a0c06c0932eb47c408915c314
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2025-05-01 09:15:48 +0200

    add grv_pseudo_random_f32

--- include/grv/grv_pseudo_random.h
+++ include/grv/grv_pseudo_random.h
@@ -9,5 +9,6 @@ u32 grv_pseudo_random_sequence_u32(u32 index);
 void grv_pseudo_random_seed_u32(u32 seed);
 u32 grv_pseudo_random_u32(void);
 i32 grv_pseudo_random_i32(i32 min_val, i32 max_val);
+f32 grv_pseudo_random_f32(void);
 
 #endif
